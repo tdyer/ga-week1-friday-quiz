1746f226a68c16535fb767bb12ea3c89e511a48f
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-27 11:07:47 -0400

    Wrote module, namespace, altered behavior methods

--- animal.rb
+++ animal.rb
@@ -1,42 +1,57 @@
+module GA
 
-# Define a parent Animal class
-# with the move behavior
-class Animal  
-  def move  
-     "i can move!"  
+
+  module Swim
+      def swim
+        puts "I can swim!"
+      end
+  end
+
+
+  # Define a parent Animal class
+  # with the move behavior
+  class Animal  
+
+    def move  
+       "i can move!"  
+    end  
   end  
-end  
 
-# Define a Mammal class that inherits from Animal
-# with the speak and eat behavior
-class Mammal < Animal
+  # Define a Mammal class that inherits from Animal
+  # with the speak and eat behavior
+  class Mammal < Animal
 
-  def eat  
-    "i can eat! yum yum yum"  
-  end 
-  def speak  
-    "i can speak"  
-  end 
-end  
-
-
-# Define a Dog class that inherits from Mammal
-# that redefines or overrides the speak behavior
-
-class Dog < Mammal  
-  
-  def initialize
-  end 
-
-  def speak 
-    "WHOOF"  
+    def eat  
+      "i can eat! yum yum yum"  
+    end 
+    def speak  
+      "i can speak"  
+    end 
   end  
-end  
 
-fido = Dog.new('fido', 5)
+  class Dolphin < Mammal
+      include Swim
+  end
+
+
+  # Define a Dog class that inherits from Mammal
+  # that redefines or overrides the speak behavior
+
+  class Dog < Mammal  
+
+    def speak 
+      "WHOOF"  
+    end  
+  end  
+
+end
+
+fido = GA::Dog.new
 puts fido.speak
 puts fido.move
 
+dolphin = GA::Dolphin.new
+puts dolphin.swim
 
 
 
