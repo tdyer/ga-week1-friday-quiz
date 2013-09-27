cd4fa712d037f7f7013897dfdaac024d4fdbad18
Abby Howell <abby.g.howell@gmail.com>
2013-09-27 10:48:36 -0400

    finished assignment

--- animal.rb
+++ animal.rb
@@ -1,41 +1,62 @@
+module GA
+
+  module Verbs
+
+    def swim
+      "breathe, kick, paddle, kick, kick, paddle, gasp..."
+    end
+
+  end
 
 # Define a parent Animal class
 # with the move behavior
-class Animal  
-  def move  
-     "i can move!"  
+  class Animal  
+    def move  
+       "i can move!"  
+    end  
   end  
-end  
 
-# Define a Mammal class that inherits from Animal
-# with the speak and eat behavior
-class Mammal < Animal
+  # Define a Mammal class that inherits from Animal
+  # with the speak and eat behavior
+  class Mammal < Animal
 
-  def eat  
-    "i can eat! yum yum yum"  
-  end 
-  def speak  
-    "i can speak"  
-  end 
-end  
-
-
-# Define a Dog class that inherits from Mammal
-# that redefines or overrides the speak behavior
-
-class Dog < Mammal  
-  
-  def initialize
-  end 
-
-  def speak 
-    "WHOOF"  
+    def eat  
+      "i can eat! yum yum yum"  
+    end 
+    def speak  
+      "i can speak"  
+    end 
   end  
-end  
 
-fido = Dog.new('fido', 5)
+  class Dolphin < Mammal
+    include Verbs
+    def move
+      swim
+    end
+  end
+
+
+
+  # Define a Dog class that inherits from Mammal
+  # that redefines or overrides the speak behavior
+
+  class Dog < Mammal  
+    
+    def initialize
+    end 
+
+    def speak 
+      "WHOOF"  
+    end  
+  end  
+end
+
+fido = GA::Dog.new
 puts fido.speak
 puts fido.move
+
+flipper = GA::Dolphin.new
+puts flipper.move
 
 
 
