37590882c0bfe89469d4329388ec3c53cad424da
Blake Ruddock <ruddock.blake@gmail.com>
2013-09-27 10:59:30 -0400

    Final commit

--- animal.rb
+++ animal.rb
@@ -1,41 +1,60 @@
+module GA
 
 # Define a parent Animal class
 # with the move behavior
-class Animal  
-  def move  
-     "i can move!"  
+  module Swim
+    def swim
+    "I can swim!"
+    end
+  end
+
+  class Animal  
+    def move  
+       "i can move!"  
+    end  
   end  
-end  
 
 # Define a Mammal class that inherits from Animal
 # with the speak and eat behavior
-class Mammal < Animal
+  class Mammal < Animal
 
-  def eat  
-    "i can eat! yum yum yum"  
-  end 
-  def speak  
-    "i can speak"  
-  end 
-end  
+    def eat  
+      "i can eat! yum yum yum"  
+    end 
+    def speak  
+      "i can speak"  
+    end 
+  end  
+
+
+  class Dolphin < Animal
+    include Swim
+
+  end  
 
 
 # Define a Dog class that inherits from Mammal
 # that redefines or overrides the speak behavior
 
-class Dog < Mammal  
-  
-  def initialize
-  end 
+  class Dog < Mammal  
+    
+    def initialize
+    end 
 
-  def speak 
-    "WHOOF"  
+    def speak 
+      "WHOOF"  
+    end  
   end  
-end  
 
-fido = Dog.new('fido', 5)
+end
+
+fido = GA::Dog.new
 puts fido.speak
 puts fido.move
+
+dolphin1 = GA::Dolphin.new
+puts dolphin1.swim
+
 
 
 
