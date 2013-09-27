a47a423b541f75f97655eecc52841481ad2a5a2e
Dan Crews <dancrews@me.com>
2013-09-27 11:37:58 -0400

    9-27 quiz submission

--- animal.rb
+++ animal.rb
@@ -1,46 +1,64 @@
+# Create a namespace
+module AnimalSpace
 
-# Define a parent Animal class
-# with the move behavior
-class Animal  
-  def move  
-     "i can move!"  
-  end  
-end  
+    # Add swim module
+    module Swimmers
+      def Swimmers.swimming
+        "swim swim swim"
+      end
+    end
 
-# Define a Mammal class that inherits from Animal
-# with the speak and eat behavior
-class Mammal < Animal
+    # Define a parent Animal class
+    # with the move behavior
+    class Animal  
+      def move  
+         "i can move!"  
+      end  
+    end  
 
-  def eat  
-    "i can eat! yum yum yum"  
-  end 
-  def speak  
-    "i can speak"  
-  end 
-end  
+    # Define a Mammal class that inherits from Animal
+    # with the speak and eat behavior
+    class Mammal < Animal
 
+      def eat  
+        "i can eat! yum yum yum"  
+      end 
 
-# Define a Dog class that inherits from Mammal
-# that redefines or overrides the speak behavior
+      def speak  
+        "i can speak"  
+      end 
+    end  
 
-class Dog < Mammal  
-  
-  def initialize
-  end 
+    class Dolphin < Animal
+    include Swimmers
 
-  def speak 
-    "WHOOF"  
-  end  
-end  
+      def initialize
+      end
 
-fido = Dog.new('fido', 5)
-puts fido.speak
-puts fido.move
+      def swim
+        Swimmers::swimming
+      end
+    end
+    # Define a Dog class that inherits from Mammal
+    # that redefines or overrides the speak behavior
 
+    class Dog < Mammal  
 
+      def initialize(name, age)
+        @name = name
+        @age = age
+      end 
 
+      def speak 
+        "WHOOF"  
+      end  
+    end  
 
+  fido = Dog.new('fido', 5)
+  puts fido.speak
+  puts fido.move
 
+  flipper = Dolphin.new
+  puts flipper.swim
 
-
-
+end
