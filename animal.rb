73d86ca371483f9b41040e5581d088818e6ee1a6
Bryce Lambert <brycelambert@gmail.com>
2013-09-27 10:54:47 -0400

    in the namespace

--- animal.rb
+++ animal.rb
@@ -1,46 +1,51 @@
-
-# Define a parent Animal class
-# with the move behavior
-class Animal  
-  def move  
-     "i can move!"  
-  end  
-end  
-
-# Define a Mammal class that inherits from Animal
-# with the speak and eat behavior
-class Mammal < Animal
-
-  def eat  
-    "i can eat! yum yum yum"  
-  end 
-  def speak  
-    "i can speak"  
-  end 
-end  
+module GA
+  module AnimalCanSwim
+    def swim
+      "I can swim"
+    end
+  end
 
 
-# Define a Dog class that inherits from Mammal
-# that redefines or overrides the speak behavior
+  # Define a parent Animal class
+  # with the move behavior
+  class Animal  
+    def move  
+       "i can move!"  
+    end  
+  end
 
-class Dog < Mammal  
-  
+  class Dolphin < Animal
+    include AnimalCanSwim
+  end
+
+  # Define a Mammal class that inherits from Animal
+  # with the speak and eat behavior
+  class Mammal < Animal
+
+    def eat  
+      "i can eat! yum yum yum"  
+    end 
+    def speak  
+      "i can speak"  
+    end 
+  end
+
+  # Define a Dog class that inherits from Mammal
+  # that redefines or overrides the speak behavior
+
+  class Dog < Mammal  
+    
   def initialize
-  end 
+    end
+    def speak 
+      "WHOOF"  
+    end  
+  end
+end
 
-  def speak 
-    "WHOOF"  
-  end  
-end  
-
-fido = Dog.new('fido', 5)
+fido = GA::Dog.new
 puts fido.speak
 puts fido.move
 
-
-
-
-
-
-
-
+flipper = GA::Dolphin.new
+puts flipper.swim
